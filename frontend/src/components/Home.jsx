35d9cdcb201d7062b2e244fec9322875d48c048d
harmanpreet07 <harmansidhu2056@gmail.com>
2025-03-31 08:28:36 +0530

    first commit

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+import Navbar from './shared/Navbar'
+import HeroSection from './HeroSection'
+import CategoryCarousel from './CategoryCarousel'
+import LatestJobs from './LatestJobs'
+import Footer from './shared/Footer'
+import useGetAllJobs from '@/hooks/useGetAllJobs'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+const Home = () => {
+  useGetAllJobs();
+  const { user } = useSelector(store => store.auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user?.role === 'recruiter') {
+      navigate("/admin/companies");
+    }
+  }, []);
+  return (
+    <div className='bg-gradient-to-r from-teal-400 to-yellow-200'>
+      <Navbar />
+      <HeroSection />
+      <CategoryCarousel />
+      <LatestJobs />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
